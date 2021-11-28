8eae8cf21590a29c5c95404d75f95a165cb03e86
Maxim Mitin <maximus405@gmail.com>
2021-11-28 18:12:40 +0300

    Initial Commit

---
+++ realmTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  realmTableView
+//
+//  Created by Maxim Mitin on 28.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
